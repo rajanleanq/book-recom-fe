a6ceb6956a1df4f9acb119257779515dfff661d7
rajan2023 <rajanshah@Rajans-MacBook-Air.local>
2024-03-26 20:35:19 +0545

    kashd

--- src/store/features/ratings/rating.api.ts
+++ src/store/features/ratings/rating.api.ts
@@ -6,21 +6,29 @@ import { AddRatingInterface, GetBookRatingInterface } from "./rating.interface";
 export const ratingApi = createApi({
   baseQuery: protectedBaseQuery,
   reducerPath: "ratingApi",
+  tagTypes:['rating'],
   endpoints: (build) => ({
     addRatingToBook: build.mutation({
       query: ({ rating, review, bookId, userId }: AddRatingInterface) => ({
         url: endpoints.book.addRatingToBook,
         body: { rating, review, bookId, userId },
         method: "POST",
+        invalidatesTags: ["rating"],
       }),
     }),
     getUserRatingOnBook: build.query({
-      query: ({ userId, bookId,page_number }: GetBookRatingInterface) => ({
-        url: endpoints.book.getUserRatingOnBook({ userId, bookId,page_number }),
+      query: ({ userId, bookId, page_number }: GetBookRatingInterface) => ({
+        url: endpoints.book.getUserRatingOnBook({
+          userId,
+          bookId,
+          page_number,
+        }),
         method: "GET",
+        providesTags: ["rating"],
       }),
     }),
   }),
 });
 
-export const { useAddRatingToBookMutation, useGetUserRatingOnBookQuery } = ratingApi;
+export const { useAddRatingToBookMutation, useGetUserRatingOnBookQuery } =
+  ratingApi;
