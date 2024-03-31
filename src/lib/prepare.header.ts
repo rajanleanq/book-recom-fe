f0ba9e3dd7ec58f2c525feb6078d03ac28df9cb0
rajan2023 <rajanshah@Rajans-MacBook-Air.local>
2024-03-31 17:11:41 +0545

    changes

--- src/lib/prepare.header.ts
+++ src/lib/prepare.header.ts
@@ -1,7 +1,14 @@
-import { getToken } from "./getToken";
+import { getAdminToken, getToken } from "./getToken";
 
 export const prepareProtectedHeader = (headers: Headers) => {
   const token = getToken();
+  if (token) {
+    headers.set("Authorization", "Bearer " + token);
+  }
+  return;
+}
+export const prepareAdminProtectedHeader = (headers: Headers) => {
+  const token = getAdminToken();
   if (token) {
     headers.set("Authorization", "Bearer " + token);
   }
