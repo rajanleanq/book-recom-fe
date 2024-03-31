69265e3ebf095327623e0d6621b2f39eddf002ee
RajThapa7 <jyangoraj123@gmail.com>
2024-03-31 10:51:40 +0545

    small changes to UI

--- src/components/pages/book/book-recommendations/book-recommendations.tsx
+++ src/components/pages/book/book-recommendations/book-recommendations.tsx
@@ -7,8 +7,6 @@ import {
 } from "@/store/features/book/book.api";
 import { useEffect, useState } from "react";
 import BookCard from "../book-card";
-import Link from "next/link";
-import { routes } from "@/contants/routes";
 
 export default function BookRecommendations() {
   const { data, isLoading } = useGetSavedBooksQuery({
@@ -47,7 +45,7 @@ export default function BookRecommendations() {
             list: data?.books,
           });
           setLoadingCosineSimilarity(false);
-          setRelatedCosineBooks(response?.data?.list?.data?.slice(1, 11));
+          setRelatedCosineBooks(response?.data?.list?.data);
         } catch (error) {
           console.error("Error fetching related books:", error);
           setLoadingCosineSimilarity(false);
@@ -103,39 +101,23 @@ export default function BookRecommendations() {
             Please wait a second...
           </p>
         )}
-        {relatedCosineBooks && relatedCosineBooks.length > 0 ? (
-          <div className="grid grid-cols-1 place-items-center sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
-            {relatedCosineBooks &&
-              relatedCosineBooks?.map((p: any, index: number) => (
-                <BookCard
-                  similarity={(p?.similarity * 100)?.toFixed(2)?.toString()}
-                  key={index + "recommend"}
-                  title={p?.title}
-                  rating={p?.average_rating}
-                  image={p?.image_url}
-                  author={p?.authors}
-                  language={p?.language_code}
-                  date={p?.original_publication_year}
-                  id={p?._id}
-                  bookId={p?.id}
-                />
-              ))}
-          </div>
-        ) : (
-          <>
-            {!loadingCosineSimilarity && (
-              <p className="text-lg text-black">
-                Please add books to your save list to see recommendations.
-                <Link
-                  href={routes.auth.save_book}
-                  className="text-blue-500 underline ml-2"
-                >
-                  Go to your save list
-                </Link>
-              </p>
-            )}
-          </>
-        )}
+        <div className="grid grid-cols-1 place-items-center sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
+          {relatedCosineBooks &&
+            relatedCosineBooks?.map((p: any, index: number) => (
+              <BookCard
+                similarity={(p?.similarity * 100)?.toFixed(2)?.toString()}
+                key={index + "recommend"}
+                title={p?.title}
+                rating={p?.average_rating}
+                image={p?.image_url}
+                author={p?.authors}
+                language={p?.language_code}
+                date={p?.original_publication_year}
+                id={p?._id}
+                bookId={p?.id}
+              />
+            ))}
+        </div>
       </div>
     </>
   );
