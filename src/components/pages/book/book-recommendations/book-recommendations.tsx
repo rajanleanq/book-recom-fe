9782219d07414cf23b69d4da871c1bc91178894a
RajThapa7 <jyangoraj123@gmail.com>
2024-04-01 05:54:03 +0545

    changes

--- src/components/pages/book/book-recommendations/book-recommendations.tsx
+++ src/components/pages/book/book-recommendations/book-recommendations.tsx
@@ -1,10 +1,12 @@
 import SectionTitle from "@/components/common/text/section-title";
+import { routes } from "@/contants/routes";
 import { getUser } from "@/lib/getUser";
 import {
   useCosineSimilarityRecommendationMutation,
   useGetSavedBooksQuery,
   useListRelatedBooksMutation,
 } from "@/store/features/book/book.api";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import BookCard from "../book-card";
 
@@ -101,23 +103,39 @@ export default function BookRecommendations() {
             Please wait a second...
           </p>
         )}
-        <div className="grid grid-cols-1 place-items-center sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
-          {relatedCosineBooks &&
-            relatedCosineBooks?.map((p: any, index: number) => (
-              <BookCard
-                similarity={(p?.similarity * 100)?.toFixed(2)?.toString()}
-                key={index + "recommend"}
-                title={p?.title}
-                rating={p?.average_rating}
-                image={p?.image_url}
-                author={p?.authors}
-                language={p?.language_code}
-                date={p?.original_publication_year}
-                id={p?._id}
-                bookId={p?.id}
-              />
-            ))}
-        </div>
+        {relatedCosineBooks && relatedCosineBooks.length > 0 ? (
+          <div className="grid grid-cols-1 place-items-center sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
+            {relatedCosineBooks &&
+              relatedCosineBooks?.map((p: any, index: number) => (
+                <BookCard
+                  similarity={(p?.similarity * 100)?.toFixed(2)?.toString()}
+                  key={index + "recommend"}
+                  title={p?.title}
+                  rating={p?.average_rating}
+                  image={p?.image_url}
+                  author={p?.authors}
+                  language={p?.language_code}
+                  date={p?.original_publication_year}
+                  id={p?._id}
+                  bookId={p?.id}
+                />
+              ))}
+          </div>
+        ) : (
+          <>
+            {!loadingCosineSimilarity && (
+              <p className="text-lg text-black">
+                Please add books to your save list to see recommendations.
+                <Link
+                  href={routes.auth.save_book}
+                  className="text-blue-500 underline ml-2"
+                >
+                  Go to your save list
+                </Link>
+              </p>
+            )}
+          </>
+        )}
       </div>
     </>
   );
